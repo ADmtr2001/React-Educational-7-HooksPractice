16d898ad05746d31850718ff3e7f15122c940798
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-11 15:39:33 +0200

    finish 1-prop-drilling

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,45 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 
+import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
-const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+const PersonContext = React.createContext();
+
+const ContextAPI = () => {
+  const [people, setPeople] = useState(data);
+
+  const removePerson = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
+  };
+
+  return (
+    <PersonContext.Provider value={{ removePerson }}>
+      <h3>prop drilling</h3>
+      <List people={people} />
+    </PersonContext.Provider>
+  )
 };
 
-export default PropDrilling;
+const List = ({ people }) => {
+  return <>
+    {people.map((person) => {
+      return <SinglePerson key={person.id} {...person} />
+    })}
+  </>
+};
+
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(PersonContext);
+  return (
+    <div className='item'>
+      <h4>{name}</h4>
+      <button onClick={removePerson(id)}>remove</button>
+    </div>
+  )
+};
+
+export default ContextAPI;
